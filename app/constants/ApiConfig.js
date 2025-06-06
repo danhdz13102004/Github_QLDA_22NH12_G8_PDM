a1ce3046f2a93e2ebefd26aea5f33044fb9e2300
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-06-06 12:48:09 +0700

    fix(forgot_password): update API BASE URL

--- app/constants/ApiConfig.js
+++ app/constants/ApiConfig.js
@@ -4,7 +4,7 @@
  */
 
 // Base API URL
-export const API_BASE_URL = 'http://192.168.1.19:3000';
+export const API_BASE_URL = 'http://10.0.2.2:3000';
 
 // User related endpoints
 export const API_ENDPOINTS = {
